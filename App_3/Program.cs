b0bf7d473b3833b5dd9e93529a6820ed3779ebba
Nikita Belov <lolvvto@yandex.ru>
2022-11-09 17:19:43 +0700

    Add App 3

---
+++ App_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Заполнение массива производит пользователь.
+
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите значение элемента с индексом " + i + ": ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine((i + 1) + " элемент массива: " + array[i]);
+}
